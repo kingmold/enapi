651e69712512ea8b87b17d676d34d6e3bdca3726
kingmold <ericnuno@gmail.com>
2020-12-06 22:34:15 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+from setuptools import setup
+
+setup(name='enapi',
+    version='0.01',
+    description='Common API functionality',
+    url="https://github.com/kingmold/enapi",
+    author='Eric Nuno',
+    author_email='ericnuno@gmail.com',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"))
